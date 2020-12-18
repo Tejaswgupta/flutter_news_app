c17966c2250e63dfcd8fe370044b02dd8e217731
Tejasw Gupta <guptatejasw2002@gmail.com>
2020-12-18 13:02:24 +0530

    Initial commit

---
+++ lib/src/helpers/constants.dart
@@ -0,0 +1,5 @@
+class Constant {
+  static const String newsApiKey = '2e38d357a66e4e1dbe2855546ca65340';
+  static const String baseUrl = 'https://newsapi.org/v2/';
+  static const String topHeadLine = '/top-headlines';
+}
